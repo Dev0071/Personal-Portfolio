52d35d0c65272ccca0390bab39e544ba174550b7
Dev0071 <lecturergacheru71@gmail.com>
2023-01-27 00:04:52 +0300

    worked on header

---
+++ src/components/Header/Cta.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cta() {
+  return (
+    <div className='cta'>
+        <a href="#">Dowmload cv</a>
+        <a href="#">Let's Talk</a>
+    </div>
+  )
+}
+
+export default Cta
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <header>
+    <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>John Gacheru</h1>
+      <h5 className="text-light">Fullstack developer</h5>
+    </div>    
+  </header>
   )
 }
 
