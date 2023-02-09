575806fcf736c48190baf755aa468c17b8215102
Dev0071 <lecturergacheru71@gmail.com>
2023-02-09 07:22:33 +0300

    done with about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './About.css'
 import ME from '../../assets/me-about.jpg'
 import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 const About = () => {
   return (
     <section id='About'>
@@ -14,14 +17,31 @@ const About = () => {
             <img src={ME} alt="me" />
           </div>
         </div>
-          <FaAward />
+          
         <div className="about__content">
           <div className="about__cards">
             <article className='about__card'>
+              <FaAward  className="about__icon"/>
               <h5>Experience</h5>
-              <small>2+ Yearrs of Work</small>
+              <small>2+ Years of Work</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className="about__icon"/>
+              <h5>Clients</h5>
+              <small>100+ Clients Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary  className="about__icon"/>
+              <h5>Projects</h5>
+              <small>50+ Completed</small>
             </article>
           </div>
+
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum consequuntur ad itaque sed, autem commodi expedita quibusdam dicta voluptatibus. Qui sunt corrupti unde nihil doloremque laborum exercitationem tenetur sit culpa.</p>
+
+          <a href="#Contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
