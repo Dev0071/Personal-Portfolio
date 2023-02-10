d47ba996d22ca798f38c4d1896c6fe0d278271d9
Dev0071 <lecturergacheru71@gmail.com>
2023-02-10 18:13:00 +0300

    experience

--- src/components/Expirience/Experience.jsx
+++ src/components/Expirience/Experience.jsx
@@ -10,6 +10,8 @@ const Experience = () => {
       <h2>My Experience</h2>
 
       <div className="container experience__container">
+
+        {/* ---------------------->frontend<-------------------------------------- */}
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
@@ -21,32 +23,74 @@ const Experience = () => {
 
             <article className="experience__details">
               <BsFillPatchCheckFill />
-              <h4>HTML</h4>
+              <h4>CSS</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className="experience__details">
               <BsFillPatchCheckFill />
-              <h4>HTML</h4>
+              <h4>JavaScript</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className="experience__details">
               <BsFillPatchCheckFill />
-              <h4>HTML</h4>
+              <h4>Bootstrap</h4>
               <small className='text-light'>Experienced</small>
             </article>
 
             <article className="experience__details">
               <BsFillPatchCheckFill />
-              <h4>HTML</h4>
+              <h4>Tailwind</h4>
               <small className='text-light'>Experienced</small>
             </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>React</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            
           </div>
         </div>
 
-        <div className="experience__backend"></div>
+{/* ================================>Backend <=============================== */}
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>Node JS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
 
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>MONGODB</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>PHP</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>MySQL</h4>
+              <small className='text-light'>Basic</small>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>Python</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            </div>
+          </div>
       </div>
     </section>
   )
