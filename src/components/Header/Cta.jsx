c88d5d09cc269618455b94e3267b33fc46f31059
Dev0071 <lecturergacheru71@gmail.com>
2023-04-12 14:55:06 +0300

    fixed a broken link

--- src/components/Header/Cta.jsx
+++ src/components/Header/Cta.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import CV from '../../assets/cv.pdf'
+import React from 'react';
+import CV from '../../assets/cv.pdf';
 
 function Cta() {
   return (
-    <div className='cta'>
-        <a href={CV} download className='btn'>Dowmload cv</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Dowmload cv
+      </a>
+      <a href="#Contact" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
-  )
+  );
 }
 
-export default Cta
+export default Cta;
