d4ec1630c26737a82e98e94ccfdcc54643df95b1
Dev0071 <lecturergacheru71@gmail.com>
2023-04-12 14:52:02 +0300

    updated headerSocial

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {FaTwitter} from 'react-icons/fa'
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
-      <a href="https://linkedin.com" target="_blank">
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/kabiru-gacheru-493bb7242"
+        target="_blank"
+        rel="noreferrer"
+      >
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com/Dev0071" rel="noreferrer" target="_blank">
         <FaGithub />
       </a>
-      <a href="https://twitter.com" target="_blank">
+      <a
+        href="https://twitter.com/KabiruGacheru?t=qLnWpopPqMs8U0u68mBFjg&s=09"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FaTwitter />
       </a>
-
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
