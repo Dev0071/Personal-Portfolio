5d4fd34b0cde009a5494bd6217e4d5e843617d7e
Dev0071 <lecturergacheru71@gmail.com>
2023-04-19 11:39:25 +0300

    fixed errors

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import './About.css'
-import ME from '../../assets/me-about.jpg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import React from 'react';
+import './About.css';
+import ME from '../../assets/john.jpg';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
 const About = () => {
   return (
-    <section id='About'>
+    <section id="About">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
@@ -17,35 +17,42 @@ const About = () => {
             <img src={ME} alt="me" />
           </div>
         </div>
-          
+
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'>
-              <FaAward  className="about__icon"/>
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>2+ Years of Work</small>
             </article>
 
-            <article className='about__card'>
-              <FiUsers className="about__icon"/>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
               <h5>Clients</h5>
               <small>100+ Clients Worldwide</small>
             </article>
 
-            <article className='about__card'>
-              <VscFolderLibrary  className="about__icon"/>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
               <small>50+ Completed</small>
             </article>
           </div>
 
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum consequuntur ad itaque sed, autem commodi expedita quibusdam dicta voluptatibus. Qui sunt corrupti unde nihil doloremque laborum exercitationem tenetur sit culpa.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            consequuntur ad itaque sed, autem commodi expedita quibusdam dicta
+            voluptatibus. Qui sunt corrupti unde nihil doloremque laborum
+            exercitationem tenetur sit culpa.
+          </p>
 
-          <a href="#Contact" className='btn btn-primary'>Let's Talk</a>
+          <a href="#Contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
