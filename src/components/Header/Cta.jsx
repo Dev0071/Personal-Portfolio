976bd5f752718dccc1e6e1aecffd4d97c11d5b4b
Dev0071 <lecturergacheru71@gmail.com>
2023-06-16 23:42:52 +0300

    restructured the code

--- src/components/Header/Cta.jsx
+++ src/components/Header/Cta.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import CV from '../../assets/JohnGacheru.pdf';
 
 function Cta() {
-  return (
-    <div className="cta">
-      <a href={CV} download className="btn">
-        Download cv
-      </a>
-      <a href="#Contact" className="btn btn-primary">
-        Let's Talk
-      </a>
-    </div>
-  );
+	return (
+		<div className='cta'>
+			<a
+				href={CV}
+				download
+				className='btn'
+			>
+				Download cv
+			</a>
+			<a
+				href='#Contact'
+				className='btn btn-primary'
+			>
+				Let's Talk
+			</a>
+		</div>
+	);
 }
 
 export default Cta;
